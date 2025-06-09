4f92ca233dfa67b27d104d66ad850ff59a0eebcd
40286439 <rlittle08@qub.ac.uk>
2025-06-09 21:42:00 +0100

    IBT32: updated template script to pull ticker symbols from csv metadata file

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -8,33 +8,36 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import config
 
 # Configuration
-ticker = "AAPL"
 start_date = "2024-12-01"
 end_date = "2024-12-31"
 price_save_path = os.path.join(config.DATA_BASE_PATH,config.PARQUET_PRICE_PATH,"AAPL.parquet")
 
-# Download price data
-data = yf.download(ticker, start = start_date, end = end_date, auto_adjust= False)
+# Retrieve tickers form metadata csv
+tickers = (pl.scan_csv(config.get_asset_metadata_path()).select("ticker").collect())
+print(tickers)
 
-# Filter relevant columns using pandas
-df_filtered = data.loc[:, [('Adj Close', 'AAPL'), ('Close', 'AAPL')]]
+# # Download price data
+# data = yf.download(ticker, start = start_date, end = end_date, auto_adjust= False)
 
-# Flatten column names
-df_filtered.columns = ['Adj Close', 'Close']
+# # Filter relevant columns using pandas
+# df_filtered = data.loc[:, [('Adj Close', 'AAPL'), ('Close', 'AAPL')]]
 
-# Reset index to return date to a regular column
-df_filtered = df_filtered.reset_index()
+# # Flatten column names
+# df_filtered.columns = ['Adj Close', 'Close']
 
-# Convert to Polars
-pl_df = pl.from_pandas(df_filtered)
+# # Reset index to return date to a regular column
+# df_filtered = df_filtered.reset_index()
 
-# Convert datetime to date
-pl_df = pl_df.with_columns(pl.col("Date").cast(pl.Date))
+# # Convert to Polars
+# pl_df = pl.from_pandas(df_filtered)
 
-print(pl_df.head(10))
+# # Convert datetime to date
+# pl_df = pl_df.with_columns(pl.col("Date").cast(pl.Date))
 
-# Ensure the directory for price_save_path exists
-os.makedirs(os.path.dirname(price_save_path), exist_ok=True)
+# print(pl_df.head(10))
 
-# Save data as parquet file
-pl_df.write_parquet(price_save_path)
+# # Ensure the directory for price_save_path exists
+# os.makedirs(os.path.dirname(price_save_path), exist_ok=True)
+
+# # Save data as parquet file
+# pl_df.write_parquet(price_save_path)
