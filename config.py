985564472a65c387289c6ed5fff8e7f55d79e037
40286439 <rlittle08@qub.ac.uk>
2025-06-09 21:18:23 +0100

    IBT31: saving cleaned data to ssd using parquet format

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-DATA_BASE_PATH = "/T7/investment_backtester_data"  
+DATA_BASE_PATH = "/Volumes/T7/investment_backtester_data"  
 METADATA_CSV = "raw/metadata/asset_metadata.csv" 
+PARQUET_PRICE_PATH = "raw/prices"
+PARQUET_DIVIDEND_PATH = "raw/dividends"
 
 def get_asset_metadata_path():
     import os
--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -1,14 +1,20 @@
+import sys    
+import os  
 import yfinance as yf
-import pandas as pd
 import polars as pl
 
+# Add the root project folder to sys.path to allow config file to be found
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))) 
+import config
+
 # Configuration
-tickers = "AAPL"
+ticker = "AAPL"
 start_date = "2024-12-01"
 end_date = "2024-12-31"
+price_save_path = os.path.join(config.DATA_BASE_PATH,config.PARQUET_PRICE_PATH,"AAPL.parquet")
 
 # Download price data
-data = yf.download(tickers, start = start_date, end = end_date, auto_adjust= False)
+data = yf.download(ticker, start = start_date, end = end_date, auto_adjust= False)
 
 # Filter relevant columns using pandas
 df_filtered = data.loc[:, [('Adj Close', 'AAPL'), ('Close', 'AAPL')]]
@@ -27,3 +33,8 @@ pl_df = pl_df.with_columns(pl.col("Date").cast(pl.Date))
 
 print(pl_df.head(10))
 
+# Ensure the directory for price_save_path exists
+os.makedirs(os.path.dirname(price_save_path), exist_ok=True)
+
+# Save data as parquet file
+pl_df.write_parquet(price_save_path)
