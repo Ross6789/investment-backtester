ba182af098d1b6068858c8ac3930d4dc12145564
40286439 <rlittle08@qub.ac.uk>
2025-06-09 19:22:40 +0100

    IBT32: cleaned api data and converted to polars dataframe

--- scripts/retrieve_data.py
+++ scripts/retrieve_data.py
@@ -1,12 +1,26 @@
 import yfinance as yf
-import os
+import pandas as pd
+import polars as pl
 
-# CONFIGURATION
+# Configuration
 tickers = ["AAPL","GOOG"]
 start_date = "2024-12-01"
 end_date = "2024-12-31"
 
-# DOWNLOAD PRICE DATA
-data = yf.download(tickers, start = start_date, end = end_date, auto_adjust= False, group_by='tickers')
-print(data)
+# Download price data
+data = yf.download(tickers, start = start_date, end = end_date, auto_adjust= False)
+
+# Filter relevant columns using pandas
+df_filtered = data.loc[:, [('Adj Close', 'AAPL'), ('Close', 'AAPL')]]
+
+# Flatten column names
+df_filtered.columns = ['Adj Close', 'Close']
+
+# Reset index to return date to a regular column
+df_filtered = df_filtered.reset_index()
+
+# Convert to Polars
+pl_df = pl.from_pandas(df_filtered)
+
+print(pl_df.head(10))
 
