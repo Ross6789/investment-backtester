1341a665aab4c010ea6f18c00809da0897e2a038
40286439 <rlittle08@qub.ac.uk>
2025-06-20 10:13:47 +0100

    added base yfinance ingestor to share common methods like batch tickers and run

--- backend/pipelines/ingestors.py
+++ backend/pipelines/ingestors.py
@@ -1,10 +1,13 @@
+from abc import ABC, abstractmethod
+from typing import List, Iterator
+import datetime
 import yfinance as yf
 import time
 import polars as pl
 import pandas as pd
 
-class YFinancePriceIngestor:
-    def __init__(self,tickers,batch_size,start_date,end_date):
+class BaseYFinanceIngestor(ABC):
+    def __init__(self, tickers: List[str], batch_size: int, start_date: datetime.date, end_date: datetime.date):
         if not isinstance(batch_size, int):
             raise TypeError("Batch size must be an integer")
         if batch_size < 1:
@@ -15,15 +18,44 @@ class YFinancePriceIngestor:
         self.start_date = start_date
         self.batch_size = batch_size
         self.end_date = end_date
-        self.data = None
+        self.data = None        
 
     # Method to batch the tickers to prevent API ban or freezing (_ is convention for private methods)
-    def _batch_tickers(self):
+    def _batch_tickers(self) -> Iterator[List[str]]:
         for i in range(0, len(self.tickers), self.batch_size):
             yield self.tickers[i:i + self.batch_size]
 
+    # Method to run internal methods in order
+    def run(self) -> pl.DataFrame:
+        batch_data = []
+        for batch in self._batch_tickers():
+            print(f"Downloading batch {batch}")
+            try:
+                batch_data.append(self.download_data(batch))
+            except Exception as e:
+                print (f"Error downloading batch {batch} : {e}")
+                continue
+            # add 2 second delay to prevent api restrictions
+            time.sleep(2)
+        try:
+            combined_data = pd.concat(batch_data)
+        except Exception as e:
+            raise RuntimeError(f"Error during batch concatenation: {e}")
+        self.transform_data(combined_data)
+        return self.data
+    
+    @abstractmethod
+    def download_data(self, tickers_batch: List[str]) -> pd.DataFrame:
+        pass  
+    
+    @abstractmethod
+    def transform_data(self, raw_data: pd.DataFrame) -> None:
+        pass
+
+class YFinancePriceIngestor(BaseYFinanceIngestor):
+
     # Method to download data using yfinance
-    def download_data(self, tickers_batch) -> pd.DataFrame:
+    def download_data(self, tickers_batch: List[str]) -> pd.DataFrame:
         print('Starting download...')
         try:
             raw_data = yf.download(tickers_batch, self.start_date, self.end_date, auto_adjust= False)
@@ -37,7 +69,7 @@ class YFinancePriceIngestor:
     
 
     # Method to remove unnecessary columns, combine data for each ticker and convert to polars dataframe
-    def transform_data(self, raw_data: pd.DataFrame):
+    def transform_data(self, raw_data: pd.DataFrame) -> None:
         dfs = []
         
         for ticker in self.tickers:
@@ -88,24 +120,6 @@ class YFinancePriceIngestor:
         self.data = combined_data_pl
 
         print('Data cleaned')
-        
-    def run(self) -> pl.DataFrame:
-        batch_data = []
-        for batch in self._batch_tickers():
-            print(f"Downloading batch {batch}")
-            try:
-                batch_data.append(self.download_data(batch))
-            except Exception as e:
-                print (f"Error downloading batch {batch} : {e}")
-                continue
-            # add 2 second delay to prevent api restrictions
-            time.sleep(2)
-        try:
-            combined_data = pd.concat(batch_data)
-        except Exception as e:
-            raise RuntimeError(f"Error during batch concatenation: {e}")
-        self.transform_data(combined_data)
-        return self.data
     
 class CSVPriceIngestor:
     def __init__(self, ticker, source_path, start_date, end_date):
@@ -180,27 +194,10 @@ class CSVPriceIngestor:
         self.transform_data(raw_data)
         return self.data
 
-class YFinanceCorporateActionsIngestor:
-    def __init__(self,tickers,batch_size,start_date,end_date):
-        if not isinstance(batch_size, int):
-            raise TypeError("Batch size must be an integer")
-        if batch_size < 1:
-            raise ValueError("Batch size must be a positive integer")
-        if start_date > end_date:
-            raise ValueError("Start date must be after the end date")
-        self.tickers = tickers
-        self.start_date = start_date
-        self.batch_size = batch_size
-        self.end_date = end_date
-        self.data = None
-
-    # Method to batch the tickers to prevent API ban or freezing (_ is convention for private methods)
-    def _batch_tickers(self):
-        for i in range(0, len(self.tickers), self.batch_size):
-            yield self.tickers[i:i + self.batch_size]
+class YFinanceCorporateActionsIngestor(BaseYFinanceIngestor):
 
     # Method to download data using yfinance
-    def download_data(self, tickers_batch) -> pd.DataFrame:
+    def download_data(self, tickers_batch: List[str]) -> pd.DataFrame:
         print('Starting download...')
         try:
             raw_data = yf.download(tickers_batch, self.start_date, self.end_date, auto_adjust= False, actions=True)
@@ -214,7 +211,7 @@ class YFinanceCorporateActionsIngestor:
     
 
     # Method to remove unnecessary columns, combine data for each ticker and convert to polars dataframe
-    def transform_data(self, raw_data: pd.DataFrame):
+    def transform_data(self, raw_data: pd.DataFrame) -> None:
         dfs = []
         
         for ticker in self.tickers:
@@ -272,21 +269,3 @@ class YFinanceCorporateActionsIngestor:
         self.data = df_cols_cast
 
         print('Data cleaned')
-        
-    def run(self) -> pl.DataFrame:
-        batch_data = []
-        for batch in self._batch_tickers():
-            print(f"Downloading batch {batch}")
-            try:
-                batch_data.append(self.download_data(batch))
-            except Exception as e:
-                print (f"Error downloading batch {batch} : {e}")
-                continue
-            # add 2 second delay to prevent api restrictions
-            time.sleep(2)
-        try:
-            combined_data = pd.concat(batch_data)
-        except Exception as e:
-            raise RuntimeError(f"Error during batch concatenation: {e}")
-        self.transform_data(combined_data)
-        return self.data
