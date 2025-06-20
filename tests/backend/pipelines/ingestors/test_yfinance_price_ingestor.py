728f9ebfa56059a7319e299ea3a5a206438c8799
40286439 <rlittle08@qub.ac.uk>
2025-06-20 14:12:00 +0100

    fixed save data method to properly partition parquet files

--- tests/backend/pipelines/ingestors/test_yfinance_price_ingestor.py
+++ tests/backend/pipelines/ingestors/test_yfinance_price_ingestor.py
@@ -19,7 +19,7 @@ def sample_yf_download_df():
     }, index=pd.to_datetime(['2025-01-01','2025-01-02']))
 
 @pytest.fixture
-def sample_polars_df():
+def sample_pl_transformed_df():
     return pl.DataFrame({
         'date':[date(2025,1,1),date(2025,1,2),date(2025,1,1),date(2025,1,2)],
         'adj_close':[1003,1005,503,505],
@@ -57,9 +57,9 @@ def test_download_data(mock_download,sample_yf_download_df,sample_ingestor):
 
     pd_assert_frame_equal(result,expected_df)
 
-def test_transform_data(sample_yf_download_df,sample_polars_df,sample_ingestor):
+def test_transform_data(sample_yf_download_df,sample_pl_transformed_df,sample_ingestor):
     raw_data = sample_yf_download_df
-    expected_df = sample_polars_df
+    expected_df = sample_pl_transformed_df
 
     ingestor = sample_ingestor
 
@@ -69,11 +69,11 @@ def test_transform_data(sample_yf_download_df,sample_polars_df,sample_ingestor):
 
     pl_assert_frame_equal(result,expected_df)
 
-def test_run(monkeypatch, sample_yf_download_df,sample_polars_df,sample_ingestor):
+def test_run(monkeypatch, sample_yf_download_df,sample_pl_transformed_df,sample_ingestor):
 
     monkeypatch.setattr(sample_ingestor,"_batch_tickers", lambda: iter([['AAPL','GOOG']]))
     monkeypatch.setattr(sample_ingestor,"download_data",lambda batch: sample_yf_download_df)
-    monkeypatch.setattr(sample_ingestor,"transform_data", lambda df: setattr(sample_ingestor,"data",sample_polars_df))
+    monkeypatch.setattr(sample_ingestor,"transform_data", lambda df: setattr(sample_ingestor,"data",sample_pl_transformed_df))
 
     result = sample_ingestor.run()
-    pl_assert_frame_equal(result, sample_polars_df)
+    pl_assert_frame_equal(result, sample_pl_transformed_df)
