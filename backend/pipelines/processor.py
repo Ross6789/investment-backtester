62793d5e1c3aab3b05c02c3aab697805dae70827
40286439 <rlittle08@qub.ac.uk>
2025-07-01 12:54:20 +0100

    IBT-86 added forward fill method within price processor

--- backend/pipelines/processor.py
+++ backend/pipelines/processor.py
@@ -93,6 +93,36 @@ class PriceProcessor:
         print('CSV price data cleaned')
         return transformed_data.sort('date')
 
+    @staticmethod
+    def forward_fill(cleaned_prices: pl.DataFrame) -> pl.DataFrame:
+
+        if cleaned_prices.is_empty():
+            print('Warning : cleaned prices dataframe is empty')
+            return cleaned_prices
+        
+        # Get min and max dates
+        start_date = cleaned_prices.select('date').min().to_series()[0]
+        end_date = cleaned_prices.select('date').max().to_series()[0]
+
+        # Create dataframe using daterange
+        dates = pl.DataFrame({'date':pl.date_range(start_date,end_date,interval='1d', eager=True)})
+
+        # Add trading dates column
+        cleaned_prices = cleaned_prices.with_columns(pl.lit(True).alias('is_trading_day'))
+
+        # Join dates df to prices df and forward fill
+        filled_prices = (
+            dates.join(cleaned_prices, on='date', how='left')
+            .with_columns([
+                pl.col('adj_close').fill_null(strategy='forward'),
+                pl.col('close').fill_null(strategy='forward'),
+                pl.col('is_trading_day').fill_null(False)
+            ])
+            .sort('date')
+        )
+
+        return filled_prices 
+
 class CorporateActionProcessor:
 
     @staticmethod
@@ -156,36 +186,3 @@ class CorporateActionProcessor:
         return df_cols_cast.sort('date')
 
 
-
-
-
-    # def process_price_data(prices: pl.DataFrame) -> pl.DataFrame:
-
-    #     # Get min and max dates
-    #     start_date = prices.select('date').min().to_series()[0]
-    #     end_date = prices.select('date').max().to_series()[0]
-
-    #     # Create dataframe using daterange
-    #     dates = pl.DataFrame({'date':pl.date_range(start_date,end_date,interval='1d', eager=True)})
-
-    #     # Add trading dates column
-    #     prices = prices.with_columns(pl.lit(True).alias('is_trading_day'))
-
-    #     # Join dates df to prices df and forward fill
-    #     filled_prices = (
-    #         dates.join(prices, on='date', how='left')
-    #         .with_columns([
-    #             pl.col('adj_close').fill_null(strategy='forward'),
-    #             pl.col('close').fill_null(strategy='forward'),
-    #             pl.col('is_trading_day').fill_null(False)
-    #         ])
-    #         .sort('date')
-    #     )
-
-    #     return filled_prices
-
-    # def process_corporate_action_data(actions: pl.DataFrame) -> pl.DataFrame:   
-
-    #     # Place for any future logic and to maintain consistency
-
-    #     return actions
