e9920ccb3f046eb40f26f8546fbba86eac124f90
40286439 <rlittle08@qub.ac.uk>
2025-08-18 03:05:58 +0100

    Fix : Strip down production app, remove data pipeline and unnecessary modules

--- backend/data_pipeline/ingestors/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base_ingestor import BaseIngestor
-from .yfinance_ingestor import YFinanceIngestor
-from .csv_ingestor import CSVIngestor
-
-__all__ = [
-    "BaseIngestor",
-    "YFinanceIngestor",
-    "CSVIngestor",
-]
--- backend/data_pipeline/ingestors/base_ingestor.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-import pandas as pd
-
-class BaseIngestor(ABC):
-
-    @abstractmethod
-    def run(self) -> pd.DataFrame:
-        pass
